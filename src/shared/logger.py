852f54b38e912dc30b4a4618c1e7b3a556804d57
Anton Golubchikov <anton1programmist@gmail.com>
2025-06-04 16:23:43 +0700

    add hdlr

--- src/shared/logger.py
+++ src/shared/logger.py
@@ -3,6 +3,6 @@ import logging
 
 def get_log(name: str, level: int = logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger(name=name)
-    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(hdlr=logging.StreamHandler())
     logger.setLevel(level)
     return logger
